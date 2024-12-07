9472038c59451bacc5e1ee68a1ed79967c731043
Marcos <230733@utxicotepec.edu.mx>
2024-12-07 09:53:01 -0600

    Tarea Integradora 2
    Modificación de los archivos pug, todas las vistas se modificaron en basea nuestra paleta de colores, añadimos estilos a nuestro front

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./views/**/*.pug'],
+  content: ['./views/**/*.pug'], 
   theme: {
     extend: {},
+    colors: {
+      'black': '#000000',
+      'white': '#FFFFFF',
+      'greenish': '#6A8E7F',
+      'light_blue': '#6F9CEB',
+      'pink': '#ED6B86',
+      'red': '#FF0000', 
+    },
   },
   plugins: [],
 }
-
