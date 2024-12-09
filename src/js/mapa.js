11efaf0b8bcbdd1216776f6baa0cb1286e609e83
Marcos <230733@utxicotepec.edu.mx>
2024-12-08 20:07:41 -0600

    ACT-04: Cambiar la posición inicial del mapa
    Modificamos los LAT Y LNG de mapa.js dento de la carpeta js dentro de src y en esa misma ubicacion, modificamos, mapaInicio.js, modificamos mapaInicio y mapa, dentro de src

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,12 +1,10 @@
 (function () {
-
-    const lat = document.querySelector('#lat').value || 20.67444163271174;
-    const lng = document.querySelector('#lng').value || -103.38739216304566;
+    const lat = document.querySelector('#lat').value || 20.37575; // 20°22'32.7"N
+    const lng = document.querySelector('#lng').value || -97.88602; // 97°53'09.7"W
     const mapa = L.map('mapa').setView([lat, lng], 16);
     let marker;
 
     //utilizar provider y geocoder
-
     const geocodeService = L.esri.Geocoding.geocodeService();
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -18,27 +16,25 @@
         draggable: true,
         autoPan: true
     })
-        .addTo(mapa)
+        .addTo(mapa);
 
     //Detectar el movimiento del pin
     marker.on('moveend', function (e) {
-        marker = e.target
+        marker = e.target;
 
         const posicion = marker.getLatLng();
 
-        mapa.panTo(new L.LatLng(posicion.lat, posicion.lng))
+        mapa.panTo(new L.LatLng(posicion.lat, posicion.lng));
 
         //obtener informacion de las calles al soltar el pin
-
         geocodeService.reverse().latlng(posicion, 13).run(function (error, resultado) {
-            marker.bindPopup(resultado.address.LongLabel)
+            marker.bindPopup(resultado.address.LongLabel);
 
             //llenar los campos
             document.querySelector('.calle').textContent = resultado?.address?.Address ?? '';
             document.querySelector('#calle').value = resultado?.address?.Address ?? '';
             document.querySelector('#lat').value = resultado?.latlng?.lat ?? '';
             document.querySelector('#lng').value = resultado?.latlng?.lng ?? '';
-        })
-
-    })
-})()
+        });
+    });
+})();
--- src/js/mapaInicio.js
+++ src/js/mapaInicio.js
@@ -1,13 +1,11 @@
-
 (function () {
-    const lat = 20.67444163271174;
-    const lng = -103.38739216304566;
+    const lat = 20.37575; // 20°22'32.7"N
+    const lng = -97.88602; // 97°53'09.7"W
     const mapa = L.map('mapa-inicio').setView([lat, lng], 13);
 
-    let markers = new L.FeatureGroup().addTo(mapa)
+    let markers = new L.FeatureGroup().addTo(mapa);
     let propiedades = [];
-    //Filtros
-
+    
     const filtros = {
         categoria: '',
         precio: ''
@@ -16,46 +14,36 @@
     const categoriasSelect = document.querySelector('#categorias');
     const preciosSelect = document.querySelector('#precios');
 
-
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(mapa)
-
-    //Filtrado de categorias y precios
+    }).addTo(mapa);
 
     categoriasSelect.addEventListener('change', e => {
-        filtros.categoria = +e.target.value
+        filtros.categoria = +e.target.value;
         filtrarPropiedades();
-    })
+    });
 
     preciosSelect.addEventListener('change', e => {
-        filtros.precio = +e.target.value
+        filtros.precio = +e.target.value;
         filtrarPropiedades();
-    })
-
+    });
 
     const obtenerPropiedades = async () => {
         try {
-            const url = '/api/propiedades'
-            const respuesta = await fetch(url)
-            propiedades = await respuesta.json()
+            const url = '/api/propiedades';
+            const respuesta = await fetch(url);
+            propiedades = await respuesta.json();
 
-            mostrarPropiedades(propiedades)
-
+            mostrarPropiedades(propiedades);
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
 
     const mostrarPropiedades = propiedades => {
-
-        //Limpiar markers previos
-
-        markers.clearLayers()
+        markers.clearLayers();
 
         propiedades.forEach(propiedad => {
-            //Agregar los pines
-
             const marker = new L.marker([propiedad?.lat, propiedad?.lng], {
                 autoPan: true
             })
@@ -66,24 +54,24 @@
             <img src="/uploads/${propiedad?.imagen}" alt="Imagen de la propiedad ${propiedad.titulo}">
             <p class="text-gray-600 font-bold">${propiedad.precio.nombre}</p>
             <a href="/propiedad/${propiedad.id}" class="bg-indigo-600 block p-2 text-center font-bold uppercase">Ver Propiedad</a>
-            `)
+            `);
 
-            markers.addLayer(marker)
-        })
+            markers.addLayer(marker);
+        });
     }
 
     const filtrarPropiedades = () => {
-        const resultado = propiedades.filter(filtrarCategoria).filter(filtrarPrecio)
-        mostrarPropiedades(resultado)
+        const resultado = propiedades.filter(filtrarCategoria).filter(filtrarPrecio);
+        mostrarPropiedades(resultado);
     }
 
     const filtrarCategoria = (propiedad) => {
-        return filtros.categoria ? propiedad.categoriaID === filtros.categoria : propiedad
+        return filtros.categoria ? propiedad.categoriaID === filtros.categoria : propiedad;
     }
 
     const filtrarPrecio = (propiedad) => {
-        return filtros.precio ? propiedad.precioID === filtros.precio : propiedad
+        return filtros.precio ? propiedad.precioID === filtros.precio : propiedad;
     }
-    obtenerPropiedades()
-
-})()
+    
+    obtenerPropiedades();
+})();
