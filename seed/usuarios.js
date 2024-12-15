39af467481b3c5b7e5a26f7b2b7d7848712601f2
Marcos <230733@utxicotepec.edu.mx>
2024-12-15 02:04:16 -0600

    ACT-05: Subir imagenes
    Logramos podder subir imagenes desde el registro, para que el usario pueda subir una imagen y un alias de usuario, asi mismo añadimos la validacion para solo crear cuentas mayores de edad, y mejoramos los errores del archivo.pug de registro, para que estas se muestren 10 segundos y se puedan cerrar automaticamente o manualmente, añadimos un focus en el boton

--- seed/usuarios.js
+++ seed/usuarios.js
@@ -2,10 +2,10 @@ import bcrypt from 'bcrypt'
 const usuarios = [
     {
         nombre: 'Esperanza',
-        email: '',
+        email: 'esperanza@gmail.com',
         confirmado: 1,
         password: bcrypt.hashSync('password', 10)
     }
 ]
 
-export default usuarios
+export default usuarios 
